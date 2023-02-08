1cd6363c531d975ec2c0d82cbf099756564230f6
Pavel Punegov <pavel.punegov@jetbrains.com>
2023-02-08 17:14:48 +0100

    This is ^KT-56414

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,7 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 buildscript {
     // workaround for KGP build metrics reports: https://github.com/gradle/gradle/issues/20001
     project.extensions.extraProperties["kotlin.build.report.output"] = null
-    // FIXME: This is to workaround issue with missing runtime KGP dependency
+
+    // FIXME: This is to workaround issue with missing runtime KGP dependency. See KT-56414
     project.extensions.extraProperties["_kgp_internal_kotlin_compile_transforms_registered"] = true
 
     val versionPropertiesFile = project.rootProject.projectDir.parentFile.resolve("gradle/versions.properties")
