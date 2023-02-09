e5303f2fa32f456bf61b7b78adf8a3c528090ba9
Simon Ogorodnik <simon.ogorodnik@jetbrains.com>
2023-02-09 01:01:18 +0100

    Modularized tests. Support configuring known compiler plugins in FP test
    Construct compiler plugins classpath based on original
    compiler arguments
    Support of plugins in full pipeline test allows us to test projects
    where some common compiler plugins are used without excluding modules

--- compiler/fir/modularized-tests/build.gradle.kts
+++ compiler/fir/modularized-tests/build.gradle.kts
@@ -30,6 +30,8 @@ dependencies {
     testApi(project(":compiler:fir:semantics"))
     testApi(project(":compiler:fir:dump"))
 
+    testRuntimeOnly(project(":compiler:fir:plugin-utils"))
+
     val asyncProfilerClasspath = project.findProperty("fir.bench.async.profiler.classpath") as? String
     if (asyncProfilerClasspath != null) {
         testRuntimeOnly(files(*asyncProfilerClasspath.split(File.pathSeparatorChar).toTypedArray()))
